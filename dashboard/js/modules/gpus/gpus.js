afe599bd76d0383d686867b1a2515aa3d2b59cfa
Saul Crumpton <saul.crumpton@gmail.com>
2020-04-02 10:29:39 -0400

    removing some of the funtions in the gpus.js file to rely on the job-maps functions instead

--- dashboard/js/modules/gpus/gpus.js
+++ dashboard/js/modules/gpus/gpus.js
@@ -29,8 +29,9 @@ define([
   'ajax-utils',
   '2d-gpudraw',
   '2d-legend-draw',
-  'jobs-utils'
-], function($, async, Handlebars, template, modalCoreTemplate, modalNodeTemplate, tokenUtils, ajaxUtils, D2Draw, d2LegendDraw, jobs) {
+  'jobs-utils',
+  'jobs-map'
+], function($, async, Handlebars, template, modalCoreTemplate, modalNodeTemplate, tokenUtils, ajaxUtils, D2Draw, d2LegendDraw, jobs,jobMap) {
   var draw = new D2Draw();
 
   template = Handlebars.compile(template);
@@ -43,66 +44,17 @@ define([
     this.config = draw.getConfig();
     this.scrollTop = 0;
 
-    function closeModalCore(e) {
-      e.stopPropagation();
-
-      $('#modal-core').remove();
-    }
-
-    function closeModalNode(e) {
-      e.stopPropagation();
-
-      $('#modal-node').remove();
-    }
-
-    function toggleModalCore(jobId) {
-
-      $.ajax(config.cluster.api.url + config.cluster.api.path + '/job/' + jobId, ajaxUtils.getAjaxOptions(config.cluster))
-        .success(function(job) {
-          var context = {
-            jobId: jobId,
-            job: job
-          };
-
-          $('body').append(modalCoreTemplate(context));
-          $('#modal-core').on('hidden.bs.modal', closeModalCore);
-          $('#modal-core').modal('show');
-        });
-    }
-
-    function toggleModalNode(nodeId) {
-
-      $.ajax(config.cluster.api.url + config.cluster.api.path + '/jobs-by-node/' + nodeId, ajaxUtils.getAjaxOptions(config.cluster))
-        .success(function(jobs) {
-          var context;
-
-          // expand the first job's informations
-          if (Object.keys(jobs).length) {
-            jobs[Object.keys(jobs)[0]].expanded = 'in';
-          }
-
-          context = {
-            count: Object.keys(jobs).length,
-            nodeId: nodeId,
-            jobs: jobs
-          };
-
-          $('body').append(modalNodeTemplate(context));
-          $('#modal-node').on('hidden.bs.modal', closeModalNode);
-          $('#modal-node').modal('show');
-        });
-    }
 
     $(document).on('modal-core', function(e, options) {
       e.stopPropagation();
 
-      toggleModalCore(options.jobId);
+      jobMap.toggleModalCore(options.jobId);
     });
 
     $(document).on('modal-node', function(e, options) {
       e.stopPropagation();
 
-      toggleModalNode(options.nodeId);
+      jobMap.toggleModalNode(options.nodeId);
     });
 
     this.saveUI = function () {
